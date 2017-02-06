171a244ff1719cd0c74231342561b18370065435
alexnovikdev <alexnovikdev@gmail.com>
2017-02-07 00:38:07 +0300

    fix

--- test.rb
+++ test.rb
@@ -2,9 +2,13 @@ require 'open-uri'
 require 'nokogiri'
 require 'csv'
 
-url = 'http://www.petsonic.com/es/perros/snacks-y-huesos-perro/galletas-granja-para-perro'
-# url = 'http://www.petsonic.com/es/perros/snacks-y-huesos-perro/estrellita-tiernas-de-salmon-para-perro'
-# url = 'http://www.petsonic.com/es/perros/juguetes-y-ocio-perro/pelotas-y-lanzaderas/kong-air-ii'
+print "Введите имя файла для записи: "
+filename = gets.strip.to_s + ".csv"
+
+
+# категория файла ввиде url
+ url = 'http://www.petsonic.com/es/perros/snacks-y-huesos-perro'
+# url = 'http://www.petsonic.com/es/perros/comida-humeda'
 
 html = open(url)
 doc = Nokogiri::HTML(html)
@@ -12,37 +16,58 @@ doc = Nokogiri::HTML(html)
 weight = ''
 price_old = ''
 price_kg = ''
+productName = ''
+imageUrl = ''
 
-doc.css(".attribute_labels_lists").each do |element|
-  element.css("input[type = 'radio']").each do |item|
+
+
+doc.css(".product_img_link").each do |product|
+
+  htmlInside = open(product["href"])
+  docInside = Nokogiri::HTML(htmlInside)
+
+  docInside.css(".attribute_labels_lists").each do |element|
+    element.css("input[type = 'radio']").each do |item|
       if item["checked"] == 'checked'
         weight =  element.css(".attribute_name").text.strip
         price_old =  element.css(".attribute_old_price").text.strip
         price_kg =  element.css(".attribute_price").text.strip
       end
+    end
+  end
+
+  productName = docInside.css('#bigpic')[0]['title'] + " - " + weight
+  imageUrl =  docInside.css('#bigpic')[0]['src']
+
+  CSV.open(filename, "a") do |wr|
+    wr << [productName, price_kg, imageUrl]
   end
 end
-
-productName = doc.css('#bigpic')[0]['title'] + " - " + weight
-imageUrl =  doc.css('#bigpic')[0]['src']
 
 # implemented with XPath
 
 =begin
-doc.xpath(".//*[@class='attribute_labels_lists']").each do |element|
-  element.xpath(".//*[@type='radio']").each do |item|
-    if item["checked"] == 'checked'
-      weight =  element.xpath(".//*[@class='attribute_name']").text.strip
-      price_old =  element.xpath(".//*[@class='attribute_old_price']").text.strip
-      price_kg =  element.xpath(".//*[@class='attribute_price']").text.strip
+doc.xpath(".//*[@class='product_img_link']").each do |product|
+
+  htmlInside = open(product["href"])
+  docInside = Nokogiri::HTML(htmlInside)
+
+  docInside.xpath(".//*[@class='attribute_labels_lists']").each do |element|
+    element.xpath(".//*[@type='radio']").each do |item|
+      if item["checked"] == 'checked'
+        weight =  element.xpath(".//*[@class='attribute_name']").text.strip
+        price_old =  element.xpath(".//*[@class='attribute_old_price']").text.strip
+        price_kg =  element.xpath(".//*[@class='attribute_price']").text.strip
+      end
     end
   end
-end
 
-productName = doc.xpath(".//*[@id='bigpic']/@title").to_s + " - " + weight
-imageUrl =  doc.xpath(".//*[@id='bigpic']/@src")
+  productName = docInside.xpath(".//*[@id='bigpic']/@title").to_s + " - " + weight
+  imageUrl =  docInside.xpath(".//*[@id='bigpic']/@src")
+
+  CSV.open(filename, "a") do |wr|
+    wr << [productName, price_kg, imageUrl]
+  end
+
+end
 =end
-
-CSV.open("data.csv", "w") do |wr|
-  wr << [productName, price_kg, imageUrl]
-end
